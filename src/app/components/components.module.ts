200312e1b9cc46852a868de9a8ea4f417f715ad2
tutosrive <tutosrivegamer@gmail.com>
2025-06-08 16:19:20 -0500

    Update login google

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,34 +1,3 @@
-// import { NgModule } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { SidebarComponent } from './sidebar/sidebar.component';
-// import { NavbarComponent } from './navbar/navbar.component';
-// import { FooterComponent } from './footer/footer.component';
-// import { RouterModule } from '@angular/router';
-// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-// import { ProfileComponent } from './profile/profile.component';
-// import { LoginButtonComponent } from './login-button/login-button.component';
-
-// @NgModule({
-//   imports: [
-//     CommonModule,
-//     RouterModule,
-//     NgbModule
-//   ],
-//   declarations: [
-//     FooterComponent,
-//     NavbarComponent,
-//     SidebarComponent,
-//     ProfileComponent,
-//     LoginButtonComponent
-//   ],
-//   exports: [
-//     FooterComponent,
-//     NavbarComponent,
-//     SidebarComponent
-//   ]
-// })
-// export class ComponentsModule { }
-
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
